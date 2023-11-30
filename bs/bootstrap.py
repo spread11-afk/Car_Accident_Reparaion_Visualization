b82ff18cc0eed9b7327d84511c69986250d3b7fd
spread11 <yyhhnnjj@gmail.com>
2023-11-30 12:46:12 +0800

    a

--- bs/bootstrap.py
+++ bs/bootstrap.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from flask import Blueprint,render_template
-from . import datasource
+#from . import datasource
 bp = Blueprint('bs', __name__, url_prefix='/bs')
 @bp.route("/")
 def index():
@@ -8,10 +8,7 @@ def index():
 
 @bp.route("/test1")
 def test():
-    data:list[tuple]=datasource.lastest_datetime_data()
-    dataFrame=pd.DataFrame(data)
-    print(dataFrame)
-    return render_template("bs/test1.html",data=data)
+    return render_template("bs/test1.html")
 
 @bp.route("/product")
 def product():
--- bs/password.py
+++
@@ -1,5 +0,0 @@
-DATABASE = 'maindb_joy0'
-USER = 'maindb_joy0_user'
-PASSWORD = '75TyvSml6DefKJkxPtxBYvIJjZdVr0Km'
-HOST = 'dpg-cl5g4tl6fh7c73empgh0-a.singapore-postgres.render.com'
-PORT = '5432'
